a454b0d1231a50d4cdb530c9784c4ed947c9f3f6
dnavmoy <dnavmoy119@g.educaand.es>
2024-01-11 11:16:02 +0100

    11-1-24 prerecreo

--- src/main/java/Catalogo/CatalogoLibros.java
+++ src/main/java/Catalogo/CatalogoLibros.java
@@ -4,6 +4,8 @@
  */
 package Catalogo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author daniel
@@ -12,5 +14,20 @@ public class CatalogoLibros {
     
     public static void main(String[] args) {
         
-    }
+        
+        Libro l1 = new Libro("123", "JRJ", "Platero y yo","planeta", 123);
+        Libro l2 = new Libro(l1);
+        Libro l3 = new Libro("1234", "JRJ", "Platero y yo","planeta", 123);
+        
+        ArrayList<Libro> lista = new ArrayList<>();
+        
+        
+//        System.out.println(l1);
+//        System.out.println(l2);
+//        System.out.println("son iguales ?" + l1.equals(l2));
+//        System.out.println(l1.hashCode());
+//        System.out.println(l2.hashCode());
+//        System.out.println("son iguales l1 y l3 " + l1.equals(l3));
+//        System.out.println("hashcode l3: " + l3.hashCode());
+//    }
 }
